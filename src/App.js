67cc18bc3ccf160a8afd5254c0558a1ab80b7470
Imikan10 <basseyimikan10@gmail.com>
2024-04-30 10:00:01 +0100

    a sliding list of workers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+
+import { names } from './data'
 
 function App() {
+  const [index, setIndex] = useState(0)
+  const [direction, setDirection] = useState('right')
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDirection('right')
+      setIndex((prevIndex) =>
+        prevIndex === names.length - 1 ? 0 : prevIndex + 1
+      )
+    }, 5000)
+
+    return () => clearInterval(interval)
+  }, [])
+
+  const handlePrevClick = () => {
+    setDirection('left')
+    setIndex((prevIndex) =>
+      prevIndex === 0 ? names.length - 1 : prevIndex - 1
+    )
+  }
+
+  const handleNextClick = () => {
+    setDirection('right')
+    setIndex((prevIndex) =>
+      prevIndex === names.length - 1 ? 0 : prevIndex + 1
+    )
+  }
+
+  // const nextIndex = index === names.length - 1 ? 0 : index + 1
+  const currentName = names[index]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <section className='cont'>
+        <article key={index} className={`current enter-${direction}`}>
+          <img src={currentName.img} alt='bassey imikan' className='img' />
+          <h5>{currentName.name}</h5>
+          <p className='status'>{currentName.status}</p>
+          <p className='info'>{currentName.info}</p>
+          <svg
+            stroke='currentColor'
+            fill='currentColor'
+            stroke-width='0'
+            viewBox='0 0 512 512'
+            className='icon'
+            height='1em'
+            width='1em'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path d='M464 32H336c-26.5 0-48 21.5-48 48v128c0 26.5 21.5 48 48 48h80v64c0 35.3-28.7 64-64 64h-8c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24h8c88.4 0 160-71.6 160-160V80c0-26.5-21.5-48-48-48zm-288 0H48C21.5 32 0 53.5 0 80v128c0 26.5 21.5 48 48 48h80v64c0 35.3-28.7 64-64 64h-8c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24h8c88.4 0 160-71.6 160-160V80c0-26.5-21.5-48-48-48z'></path>
+          </svg>
+        </article>
+
+        <button type='button' className='prev' onClick={handlePrevClick}>
+          <svg
+            stroke='currentColor'
+            fill='none'
+            stroke-width='2'
+            viewBox='0 0 24 24'
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            height='1em'
+            width='1em'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <polyline points='15 18 9 12 15 6'></polyline>
+          </svg>
+        </button>
+        <button type='button' className='next' onClick={handleNextClick}>
+          <svg
+            stroke='currentColor'
+            fill='none'
+            stroke-width='2'
+            viewBox='0 0 24 24'
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            height='1em'
+            width='1em'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <polyline points='9 18 15 12 9 6'></polyline>
+          </svg>
+        </button>
+      </section>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/data.js
@@ -0,0 +1,30 @@
+export const names = [
+  {
+    name: 'bassey imikan',
+    status: 'the boss',
+    info: ' ipsam iure a iste odio doloremque minus minima veritatis adipiscinatus ullam voluptatum hic perspiciatis assumenda earum voluptatem quibusdam error! Iusto.',
+    img: 'https://www.course-api.com/images/people/person-3.jpeg',
+    index: 1,
+  },
+  {
+    name: 'miracle chegbene',
+    status: 'office manager',
+    info: 'Enim ipsum nihil quaerat similique iure nisi iusto magnam error quia, illo animi nam accusamus modi consequatur ipsa ducimus? Dicta, iure iste.',
+    img: 'https://www.course-api.com/images/people/person-1.jpeg',
+    index: 2,
+  },
+  {
+    name: 'olisa anslem',
+    status: 'regular guy',
+    info: 'consectetur adipisicing. Delectus, consectetur adipisicing elit. Enim ipsum nihil quaerat similique iure nisi iusto',
+    img: 'https://www.course-api.com/images/people/person-2.jpeg',
+    index: 3,
+  },
+  {
+    name: 'ogunrombi emmanuel',
+    status: 'product designer',
+    info: 'sit amet consectetur adipisicing elit.expedita, officia provident numquam minus omnis voluptatum, itaque explicabo fugiat assumenda? Reprehenderit itaque aperiam sequi dolor.',
+    img: 'https://www.course-api.com/images/people/person-4.jpeg',
+    index: 4,
+  },
+]
